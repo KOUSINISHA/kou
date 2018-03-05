46bb20889ee3acca42009f74958b7ed0795907d5
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-05 16:17:04 +0530

    hello.c
    good

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n,m;
+printf("Enter the N number");
+scanf("%d%d,& N");
+printf("Enter the M number");
+scanf("%d%d,& M");
+}
+printf("Hello");
+}
+getch();
+}
